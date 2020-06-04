cc8a758fda7bb10180862b457fc00b5cf7da318c
Thomas Sihapanya <sihapanya.thomas@gmail.com>
2020-06-04 14:19:56 +0200

    CRUD for everything

--- src/DataFixtures/ReviewFixtures.php
+++ src/DataFixtures/ReviewFixtures.php
@@ -2,16 +2,45 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Review;
+use App\Repository\RestaurantRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
-class ReviewFixtures extends Fixture
+class ReviewFixtures extends Fixture implements DependentFixtureInterface
 {
+
+    private $restaurantRepository;
+
+    public function __construct(RestaurantRepository $restaurantRepository) {
+        $this->restaurantRepository = $restaurantRepository;
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
+
+        for ($i = 0; $i < 2000; $i++) {
+
+            $restaurant = $this->restaurantRepository->find( rand(1, 100) );
+
+            $review = new Review;
+            $review->setName($faker->sentence);
+            $review->setContent($faker->realText(500));
+            $review->setRating( rand(1, 5) );
+            $review->setRestaurant($restaurant);
+            $manager->persist($review);
+        }
 
         $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+            RestaurantFixtures::class
+        ];
     }
 }
---
+++ src/Form/ReviewType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Review;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ReviewType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('content')
+            ->add('rating')
+            ->add('restaurant')
+            ->add('user')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Review::class,
+        ]);
+    }
+}
